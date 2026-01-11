32efd6d5740c101ef0aa2c2f7603447211af09fa
Suraj <kunwarsuraj19@gmail.com>
2026-01-11 10:37:13 +0530

    optional also completed

--- optional_pract.java
+++ optional_pract.java
@@ -1,5 +1,15 @@
+import java.util.Optional;
+
 public class optional_pract {
     public static void main(String[] args) {
+      Optional<String> name = findName(2);
+      name.ifPresent(System.out::println);
 
     }
+
+    private  static Optional<String> findName(int id){
+        return Optional.of("Suraj");
+        //return Optional.ofNullable(null);
+    }
+
 }
